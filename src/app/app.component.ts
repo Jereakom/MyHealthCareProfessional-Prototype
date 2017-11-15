c7c2be3ab3fe0fdb23918b296f99d5d09b63241c
Jere Komminaho <jereakom@gmail.com>
2017-11-15 15:14:29 +0200

    Updated proto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My Health Care Professional';
+  title = 'Healthmatch';
   logo ='company logo';
 }
